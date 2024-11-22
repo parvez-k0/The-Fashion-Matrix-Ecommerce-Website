1446dbea7ee86a0da433d84cee1d1a55ce7530a0
parvez-k0 <parvezkh103@gmail>
2024-11-22 17:49:23 +0530

    added python project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\Users\Lenovo\OneDrive\Desktop\Source Code\AstroDjango\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
